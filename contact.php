586bca396e2c822e0f9427ba4bab27af7210c793
Abhinandan Prajapati <103814181+sdeabhi@users.noreply.github.com>
2022-05-24 22:38:01 +0530

    Update contact.php
    as you know INSERT INTO statement used to insert values in table. So we should always use 'INSERT INTO' instead of 'insert into'.

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ if(isset($_POST['btnSubmit']))
 	$contact = $_POST['txtPhone'];
 	$message = $_POST['txtMsg'];
 
-	$query="insert into contact(name,email,contact,message) values('$name','$email','$contact','$message');";
+	$query="INSERT INTO contact(name,email,contact,message) values('$name','$email','$contact','$message')";
 	$result = mysqli_query($con,$query);
 	
 	if($result)
